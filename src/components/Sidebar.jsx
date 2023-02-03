4be5134d8baaec93d3ecaa520e78c3ea1545957e
ghazytp <ghazytp@gmail.com>
2023-02-03 11:07:29 +0700

    sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+export default function Sidebar() {
+    return (
+        <>
+        <section className="">
+            SIDEBAR
+        </section>
+        </>
+    )
+}
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,10 +1,14 @@
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 export default function Dashboard() {
-    return (
-        <>
-        <Navbar/>
+  return (
+    <>
+      <Navbar />
+      <div className="flex">
+        <Sidebar />
         <div>HELLO WORLD</div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
